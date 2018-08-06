fe1b8e3ce24b600f0f8fcdc069350077a12c2778
WilliamMarti <kareas1115@gmail.com>
2018-08-05 22:11:01 -0700

    add templates

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+
+	# Router1
+	name = models.CharField(max_length=30)
+
+	# router/switch/AP
+	device_type = models.CharField(max_length=30)
+
+	# ISR4331/CAT2960
+	model = models.CharField(max_length=30)
+
+	# New York/San Diego
+	site = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
+
+class User(models.Model):
+
+	username = models.CharField(max_length=30)
+
+	firstname = models.CharField(max_length=30)
+
+	lastname = models.CharField(max_length=30)
+
+	email = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.username
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from core.models import Device
+
 # Create your views here.
 
 
@@ -10,7 +12,24 @@ def index(request):
 	page = "Home"
 	title = "Confgr - " + page
 
+	devices = Device.objects.all()
+
 	context = {'username': username,
-				'title': title}
+				'title': title,
+				'devices': devices}
 
 	return render(request, 'core/home.html', context)
+
+def inventory(request):
+
+	username = 'wmarti'
+	page = "Inventory"
+	title = "Confgr - " + page
+
+	devices = Device.objects.all()
+
+	context = {'username': username,
+				'title': title,
+				'devices': devices}
+
+	return render(request, 'core/inventory.html', context)
